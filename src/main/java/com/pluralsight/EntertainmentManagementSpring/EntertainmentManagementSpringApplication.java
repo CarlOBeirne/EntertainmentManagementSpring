b199eb4bb384a0690038b244b49b95dccf8425af
Carl OBeirne <carl.obeirne@expeditors.com>
2025-03-03 08:19:22 +0000

    Initial Commit - Project Structure

---
+++ src/main/java/com/pluralsight/EntertainmentManagementSpring/EntertainmentManagementSpringApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.EntertainmentManagementSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntertainmentManagementSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntertainmentManagementSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluralsight/EntertainmentManagementSpring/EntertainmentManagementSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.EntertainmentManagementSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntertainmentManagementSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
